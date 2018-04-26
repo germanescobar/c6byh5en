978d8dca9e4ed7c9736c506f7442fbaa33fe0b14
capaisa12 <capaisa12@gmail.com>
2018-04-25 20:10:54 -0500

    Se crea el namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'products/index'
+    end
+  end
+
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
